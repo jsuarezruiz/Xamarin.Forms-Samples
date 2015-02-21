aba18bde544b2fe2de8d01d0154b65433e98b9fd
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-02-21 18:27:20 +0100

    Conditional XAML

---
+++ XamarinConditionalXAML/XamarinConditionalXAML/XamarinConditionalXAML/App.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+using XamarinConditionalXAML.Views;
+
+namespace XamarinConditionalXAML
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new MainView();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ XamarinConditionalXAML/packages/xcc.1.0.5/ReadMe.txt
@@ -0,0 +1,5 @@
+﻿XAML CONDITIONAL COMPILATION
+============================
+Welcome to XCC, a preprocessor adding conditional compilation support to XAML files.
+
+See https://github.com/firstfloorsoftware/xcc for instructions
