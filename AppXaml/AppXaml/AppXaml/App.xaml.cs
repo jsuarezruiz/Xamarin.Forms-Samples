ae90c06b6bb8bb7539d9655b97c4f1874a7a2ef2
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-06-22 21:12:27 +0200

    Added App.xaml (HowTo) Sample

---
+++ AppXaml/AppXaml/AppXaml/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using AppXaml.Views;
+using Xamarin.Forms;
+
+namespace AppXaml
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new MainView());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
