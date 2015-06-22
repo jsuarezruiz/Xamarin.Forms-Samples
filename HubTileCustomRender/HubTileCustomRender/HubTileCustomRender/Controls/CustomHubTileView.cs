5f034584931292d09562b0116c1eceddb8840f23
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-06-22 21:18:29 +0200

    Using native controls in Xamarin.Forms App

---
+++ HubTileCustomRender/HubTileCustomRender/HubTileCustomRender/Controls/CustomHubTileView.cs
@@ -0,0 +1,39 @@
+﻿using Xamarin.Forms;
+
+namespace HubTileCustomRender.Controls
+{
+    public class CustomHubTileView : View
+    {
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create<CustomHubTileView, string>(p => p.Title, string.Empty);
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly BindableProperty MessageProperty = BindableProperty.Create<CustomHubTileView, string>(p => p.Message, string.Empty);
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public static readonly BindableProperty SourceProperty = BindableProperty.Create<CustomHubTileView, ImageSource>(p => p.Source, string.Empty);
+
+        public ImageSource Source
+        {
+            get { return (ImageSource)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public static readonly BindableProperty ColorProperty = BindableProperty.Create<CustomHubTileView, Color>(p => p.Color, Color.Default);
+
+        public Color Color
+        {
+            get { return (Color)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+    }
+}
---
+++ HyperlinkCustomRender/HyperlinkCustomRender/HyperlinkCustomRender.WinPhone/Controls/HyperLinkControlRenderer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+using HyperlinkCustomRender.Controls;
+using HyperlinkCustomRender.WinPhone.Controls;
+using Microsoft.Phone.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WinPhone;
+
+[assembly: ExportRenderer(typeof(HyperLinkControl), typeof(HyperLinkControlRenderer))]
+namespace HyperlinkCustomRender.WinPhone.Controls
+{
+    public class HyperLinkControlRenderer : ViewRenderer<HyperLinkControl, HyperlinkButton>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<HyperLinkControl> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null)
+                return;
+
+            var element = new HyperlinkButton
+            {
+                TargetName = Element.Text,
+                Content = Element.Text
+            };
+
+            element.Click += (sender, args) =>
+            {
+                if (Element.NavigateUri.Contains("@"))
+                {
+                    var emailComposeTask = new EmailComposeTask { Subject = string.Empty, To = "mailto:" + Element.NavigateUri };
+                    emailComposeTask.Show();
+                }
+                else if (Element.NavigateUri.Contains("www.") || 
+                    Element.NavigateUri.Contains("http:"))
+                {
+                    var uri = Element.NavigateUri.StartsWith("http:")
+                                  ? new Uri(Element.NavigateUri)
+                                  : new Uri(@"http://" + Element.NavigateUri);
+
+                    var webBrowserTask = new WebBrowserTask { Uri = uri };
+                    webBrowserTask.Show();
+                }
+            };
+
+            SetNativeControl(element);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if ((e.PropertyName == Label.TextProperty.PropertyName))
+                Control.Content = Element.Text;
+        }
+    }
+}
