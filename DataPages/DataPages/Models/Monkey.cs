969eb6099d7f425109d3f93a00cf0ee3b9cb10cf
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2016-05-07 13:50:48 +0200

    Xamarin Forms DataPages Sample

---
+++ DataPages/DataPages/Models/Monkey.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DataPages
+{
+	public class Monkey
+	{
+		public string Name { get; set; }
+		public string Location { get; set; }
+		public string Details { get; set; }
+		public string Image { get; set; }
+
+
+		public string NameSort
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(Name) || Name.Length == 0)
+					return "?";
+
+				return Name[0].ToString().ToUpper();
+			}
+		}
+
+	}
+}
+
---
+++ DataPages/Droid/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace DataPages.Droid
+{
+	[Activity(Label = "DataPages.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
+
+			base.OnCreate(bundle);
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+
+			var x = typeof(Xamarin.Forms.Themes.DarkThemeResources);
+			x = typeof(Xamarin.Forms.Themes.LightThemeResources);
+			x = typeof(Xamarin.Forms.Themes.Android.UnderlineEffect);
+
+			LoadApplication(new App());
+		}
+	}
+}
+
