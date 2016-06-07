95d47a753a56d1f0b90e69030a6e50844b20e06d
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2016-06-07 21:01:59 +0200

    Xamarin.Android HockeyApp Sample

---
+++ AndroidHockeyApp/AndroidHockeyApp/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.OS;
+using HockeyApp;
+
+namespace AndroidHockeyApp
+{
+    [Activity(Label = "AndroidHockeyApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        public const string HOCKEYAPP_APPID = "HOCKEYAPP_APPID";
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            CrashManager.Register(this, HOCKEYAPP_APPID);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.YourMainView);
+        }
+    }
+}
+
