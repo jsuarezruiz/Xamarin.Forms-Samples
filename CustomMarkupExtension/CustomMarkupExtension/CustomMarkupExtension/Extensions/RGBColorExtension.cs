b6ada43cf98d90d30eeaacb8e7873a7bc375e758
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-07-17 19:36:18 +0200

    Custom MarkupExtensions sample

---
+++ CustomMarkupExtension/CustomMarkupExtension/CustomMarkupExtension/Extensions/RGBColorExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CustomMarkupExtension.Extensions
+{
+    public class RGBColor : IMarkupExtension
+    {
+        public int Red { get; set; }
+
+        public int Green { get; set; }
+
+        public int Blue { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Color.FromRgb(Red, Green, Blue);
+
+        }
+    }
+}
