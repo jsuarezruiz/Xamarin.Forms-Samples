f760a101ffff7463a7f95f0e61a0d12f7330a0a5
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-08-13 12:31:35 +0200

    Mobile Center Sample

---
+++ MobileCenterCrashApp/MobileCenterCrashApp/MobileCenterCrashApp/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using MobileCenterCrashApp.Views;
+using Xamarin.Forms;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
+namespace MobileCenterCrashApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new ErrorView());
+        }
+
+        protected override void OnStart()
+        {
+            MobileCenter.Start("4a91c419-e00f-4d0e-9930-8ff34185aa54",     
+                typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ MobileCenterCrashApp/MobileCenterCrashApp/MobileCenterCrashApp/Views/ErrorView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.Mobile.Analytics;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MobileCenterCrashApp.Views
+{
+    public partial class ErrorView : ContentPage
+    {
+        public ErrorView()
+        {
+            InitializeComponent();
+
+            EventBtn.Clicked += (sender, args) =>
+            {
+                var properties = new Dictionary<string, string>
+                {
+                     { "Parameter", "Data" }
+                };
+
+                Analytics.TrackEvent("EventBtn Clicked", properties);
+            };
+
+            ExceptionBtn.Clicked += (sender, args) =>
+            {
+                throw new Exception("Exception information");
+            };
+        }
+    }
+}
