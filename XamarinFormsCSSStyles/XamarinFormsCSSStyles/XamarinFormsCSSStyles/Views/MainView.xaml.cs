10d94a3d9d3203432a4a4e6be523d349d3ac2278
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-01-02 15:29:40 +0100

    Added new Xamarin.Forms Sample using CSS Styles

---
+++ XamarinFormsCSSStyles/XamarinFormsCSSStyles/XamarinFormsCSSStyles/Views/MainView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsCSSStyles.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainView : ContentPage
+	{
+		public MainView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
