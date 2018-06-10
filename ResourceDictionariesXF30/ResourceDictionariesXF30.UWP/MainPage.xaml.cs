044212441e07a3a5a6c4e3c14d21938839663d30
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-06-10 18:49:00 +0200

    Added new Xamarin.Forms 3.0 ResourceDictionary sample

---
+++ ResourceDictionariesXF30/ResourceDictionariesXF30.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ResourceDictionariesXF30.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new ResourceDictionariesXF30.App());
+        }
+    }
+}
---
+++ ResourceDictionariesXF30/ResourceDictionariesXF30/MainView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ResourceDictionariesXF30
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainView : ContentPage
+	{
+		public MainView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
