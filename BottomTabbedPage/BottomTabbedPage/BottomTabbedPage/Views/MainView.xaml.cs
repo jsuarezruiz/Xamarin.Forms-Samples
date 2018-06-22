d7059bd41b3cfc145c80fb3f5d3f052fcbc0c1e3
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-06-22 20:06:31 +0200

    Added BottomTabbedPage Sample

---
+++ BottomTabbedPage/BottomTabbedPage/BottomTabbedPage/Views/MainView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace BottomTabbedPage.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainView : Xamarin.Forms.TabbedPage
+    {
+		public MainView ()
+		{
+			InitializeComponent ();
+
+            On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+        }
+	}
+}
