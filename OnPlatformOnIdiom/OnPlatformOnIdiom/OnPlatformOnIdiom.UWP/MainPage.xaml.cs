65a6e670270a7dba3fef1c99c8e8084762bc9539
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-09-22 12:54:41 +0200

    Added new sample, New Xamarin.Forms 3.2 OnPlatform/OnIdiom markup extension

---
+++ OnPlatformOnIdiom/OnPlatformOnIdiom/OnPlatformOnIdiom.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OnPlatformOnIdiom.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new OnPlatformOnIdiom.App());
+        }
+    }
+}
---
+++ OnPlatformOnIdiom/OnPlatformOnIdiom/OnPlatformOnIdiom/Views/MainView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OnPlatformOnIdiom.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainView : ContentPage
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
