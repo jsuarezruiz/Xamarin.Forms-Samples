01ee805d29f103c03a9db42f40157d0c2ac75654
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-10-27 18:05:43 +0200

    Added Compiled Bindings sample

---
+++ CompiledBindingsSample/CompiledBindingsSample/CompiledBindingsSample.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CompiledBindingsSample.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new CompiledBindingsSample.App());
+        }
+    }
+}
---
+++ CompiledBindingsSample/CompiledBindingsSample/CompiledBindingsSample/ViewModels/MainViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Xamarin.Forms;
+
+namespace CompiledBindingsSample.ViewModels
+{
+    public class MainViewModel : BindableObject
+    {
+        private string _value1;
+        private string _value2;
+        private string _value3;
+        private string _value4;
+        private string _value5;
+
+        public MainViewModel()
+        {
+            Value1 = "Value1";
+            Value2 = "Value2";
+            Value3 = "Value3";
+            Value4 = "Value4";
+            Value5 = "Value5";
+        }
+
+        public string Value1
+        {
+            get { return _value1; }
+            set
+            {
+                _value1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value2
+        {
+            get { return _value2; }
+            set
+            {
+                _value2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value3
+        {
+            get { return _value3; }
+            set
+            {
+                _value3 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value4
+        {
+            get { return _value4; }
+            set
+            {
+                _value4 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value5
+        {
+            get { return _value5; }
+            set
+            {
+                _value5 = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
